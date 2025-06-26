bd5a16c13450d7d63572c8394256a7c6c9afed16
Alexandra Belcher <abe150@uclive.ac.nz>
2025-06-26 23:28:49 +1200

    Tried to stabalise some of the navigation errors

--- team-organiser/app/_layout.tsx
+++ team-organiser/app/_layout.tsx
@@ -5,6 +5,9 @@ export default function RootLayout() {
   return (
   <>
     <Stack>
+        <Stack.Screen name="index" options={{headerShown: false, title:"home"}} />
+        <Stack.Screen name="RegisterScreen" options={{headerShown: false, title:"Register"}} />
+        <Stack.Screen name="login" options={{headerShown: false, title:"Login"}} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
     </Stack>
